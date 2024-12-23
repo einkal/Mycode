2c92a0d0fd371623ca82ce62ab59d5d80e4847b8
einkal <einkal4@gmail.com>
2024-12-23 22:41:04 +0530

    Added LRU

---
+++ src/com/scaler/lab/Entity/items/LRUCache.java
@@ -0,0 +1,25 @@
+package com.scaler.lab.Entity.items;
+
+import java.util.LinkedHashMap;
+
+public class LRUCache {
+    private int maxSize;
+    private LinkedHashMap<String,Item > map;
+
+    public LRUCache(int maxSize){
+        this.maxSize = maxSize;
+        this.map = new LinkedHashMap<>();
+    }
+
+    public void addRecentlyViewedItem(Item item) {
+        if(this.map.containsKey((item.getId()))) {
+            this.map.remove(item.getId());
+        }
+
+        if(this.map.size() > maxSize) {
+            this.map.remove(map.keySet().iterator().next());
+        }
+        this.map.put(item.getId(), item);
+    }
+
+}
